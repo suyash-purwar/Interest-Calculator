15f52d9c53d261e58fb171a90b4b42a19ce3d5f2
Suyash <purwarsuyash06@gmail.com>
2019-05-28 19:19:03 +0530

    Made SIForm stateful widget

--- interest_calculator/lib/main.dart
+++ interest_calculator/lib/main.dart
@@ -1,5 +1,25 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp();
+  runApp(
+    MaterialApp(
+      title: "Simple Interest Calculator App",
+      home: SIForm()
+    )
+  );
+}
+
+class SIForm extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    return _SIForm();
+  }
+}
+
+class _SIForm extends State<SIForm> {
+  List<String> _currencies = ["Rupees", "Dollars", "Pounds", "Others"];
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
 }
